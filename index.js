caa35bb9c40e7df6bd6b2edc989db9725036bb3c
tlcraft <tlcraft@users.noreply.github.com>
2018-07-11 20:24:03 -0600

    Complete initial game
    The game is playable and will display the winner

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ class Board extends React.Component {
     // immutability is important in React fundamentally
     // one key use is for comparison of the state of an object
     const squares = this.state.squares.slice();
+    
+    // return if the game is over or the square is already filled
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -44,7 +50,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -91,3 +103,25 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
